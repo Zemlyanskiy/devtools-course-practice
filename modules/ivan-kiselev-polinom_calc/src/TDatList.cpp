d81b9c16c6550aca757bbd9823b163b12b5c34fe
Umlore1998 <iv.kiselev1998@yandex.ru>
2017-05-08 23:07:56 +0300

    .cxx -> .cpp

--- modules/ivan-kiselev-polinom_calc/src/TDatList.cpp
+++ modules/ivan-kiselev-polinom_calc/src/TDatList.cpp
@@ -14,13 +14,13 @@ PTDatValue TDatList::GetDatValue()
 return pCurrLink->pValue;
 }
 
-TDatList::TDatList()			
+TDatList::TDatList()
 {
 pFirst = new TDatLink(NULL, NULL);
-pLast = pFirst;     
-pCurrLink = pFirst; 
-pPrevLink = NULL; 
-pStop = pFirst;    
+pLast = pFirst;
+pCurrLink = pFirst;
+pPrevLink = NULL;
+pStop = pFirst;
 CurrPos = 0;
 ListLen = 0;
 }
@@ -54,69 +54,69 @@ return ((CurrPos >= ListLen) || (pCurrLink == pLast));
 
 int TDatList::GoNext(void)
 {
-if (IsListEnded()) return 1;
-pPrevLink = pCurrLink;
-pCurrLink = pCurrLink->GetNextDatLink();
-CurrPos++;
-return 0;
+	if (IsListEnded()) return 1;
+	pPrevLink = pCurrLink;
+	pCurrLink = pCurrLink->GetNextDatLink();
+	CurrPos++;
+	return 0;
 }
 
 void TDatList::InsFirst(PTDatValue pVal)
 {
-pFirst = GetLink(pVal, pFirst);
-ListLen++;
+	pFirst = GetLink(pVal, pFirst);
+	ListLen++;
 }
 
 void TDatList::InsLast(PTDatValue pVal)
 {
-if (pFirst->pValue == NULL)
-{
-	InsFirst(pVal);
+	if (pFirst->pValue == NULL)
+	{
+		InsFirst(pVal);
+	}
+	else
+	{
+		while (GoNext() != 1);
+		pPrevLink->SetNextLink(GetLink(pVal, pLast));
+		pCurrLink = pPrevLink->GetNextDatLink();
+		ListLen++;
+	}
 }
-else
-{
-	while (GoNext() != 1);
-	pPrevLink->SetNextLink(GetLink(pVal, pLast));
-	pCurrLink = pPrevLink->GetNextDatLink();
-	ListLen++;
-}
-} 
 
 void TDatList::InsCurrent(PTDatValue pVal)
 {
-if (pPrevLink != NULL)
-{
-	pPrevLink->SetNextLink(GetLink(pVal, pCurrLink));
-	pCurrLink = pPrevLink->GetNextDatLink();
-	ListLen++;
-}
-else InsFirst(pVal);
+	if (pPrevLink != NULL)
+	{
+		pPrevLink->SetNextLink(GetLink(pVal, pCurrLink));
+		pCurrLink = pPrevLink->GetNextDatLink();
+		ListLen++;
+	}
+	else InsFirst(pVal);
 }
 
 void TDatList::DelFirst(void)
 {
-PTDatLink old = pFirst;
-pFirst = pFirst->GetNextDatLink();
-ListLen--;
-old->~TDatLink();
+	PTDatLink old = pFirst;
+	pFirst = pFirst->GetNextDatLink();
+	ListLen--;
+	old->~TDatLink();
 }
 
 void TDatList::DelCurrent(void)
 {
-if (pCurrLink == pFirst)
-{
-	DelFirst();
-}
-else {
-	PTDatLink old = pCurrLink;
-	pPrevLink->SetNextLink(pCurrLink->GetNextLink());
-	pCurrLink = pCurrLink->GetNextDatLink();
-	old->~TDatLink();
-	ListLen--;
-}
+	if (pCurrLink == pFirst)
+	{
+		DelFirst();
+	}
+	else {
+		PTDatLink old = pCurrLink;
+		pPrevLink->SetNextLink(pCurrLink->GetNextLink());
+		pCurrLink = pCurrLink->GetNextDatLink();
+		old->~TDatLink();
+		ListLen--;
+	}
 }
 
 void TDatList::DelList(void)
 {
-while (ListLen > 0) DelFirst();
+	while (ListLen > 0) DelFirst();
 }
