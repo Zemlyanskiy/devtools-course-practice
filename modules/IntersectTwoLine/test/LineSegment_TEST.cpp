761d492e7b206607892373e6f0c1558f8861812d
Lobanov A <1etodva@mail.ru>
2017-05-08 14:53:40 +0300

    fixed tests 3

--- modules/IntersectTwoLine/test/LineSegment_TEST.cpp
+++ modules/IntersectTwoLine/test/LineSegment_TEST.cpp
@@ -18,55 +18,43 @@ TEST(LineSegment, Can_Create_Zero_Segment_) {
   ASSERT_NO_THROW(LineSegment2D line(0, 0, 0));
 }
 
-TEST(LineSegment, Can_Set_Initial_A_Value) {
-  LineSegment2D line(0, 13.4, 11);
-  ASSERT_NO_THROW(line.Set_A(3.3));
-}
-
-TEST(LineSegment, Can_Set_Initial_B_Value) {
-  LineSegment2D line(3.2, 0, 7.0);
-  ASSERT_NO_THROW(line.Set_B(3.3));
-}
-
-TEST(LineSegment, Can_Set_Initial_C_Value) {
-  LineSegment2D line(0, 0, 0);
-  ASSERT_NO_THROW(line.Set_C(3.3));
-}
-
 TEST(LineSegment, Can_Get_A_Value) {
-  LineSegment2D line(3.0, 0, 0);
-  ASSERT_NO_THROW(line.Get_A());
+  LineSegment2D line(-33.8, -4.8, 5.1);
+  ASSERT_DOUBLE_EQ(-33.8, line.Get_A());
 }
 
 TEST(LineSegment, Can_Get_B_Value) {
-  LineSegment2D line(3.0, 3.3, 0);
-  ASSERT_NO_THROW(line.Get_B());
-}
-
-TEST(LineSegment, Can_Get_C_Value) {
-  LineSegment2D line(3.0, 0, 4.7);
-  ASSERT_NO_THROW(line.Get_C());
-}
-
-TEST(LineSegment, Get_A_Value_Works_Correctly) {
-  LineSegment2D line(-33.8, -4.8, 5.1);
-  ASSERT_DOUBLE_EQ(line.Get_A(), -33.8);
-}
-
-TEST(LineSegment, Get_B_Value_Works_Correctly) {
   LineSegment2D line(-3.0, -4.47, 5.39);
   ASSERT_DOUBLE_EQ(-4.47, line.Get_B());
 }
 
-TEST(LineSegment, Get_C_Value_Works_Correctly) {
+TEST(LineSegment, Can_Get_C_Value) {
   LineSegment2D line(-3.0, -4.0, 5.28);
   ASSERT_DOUBLE_EQ(5.28, line.Get_C());
 }
 
-TEST(LineSegment, Get_Value_Test) {
+TEST(LineSegment, Get_Value_Works_Correctly) {
   LineSegment2D line(2.22, 2.22, 2.22);
   ASSERT_TRUE(line.Get_A() == line.Get_B() &&
               line.Get_A() == line.Get_C());
+}
+
+TEST(LineSegment, Can_Set_Initial_A_Value) {
+  LineSegment2D line(0, 13.4, 11);
+  line.Set_A(3.3);
+  ASSERT_DOUBLE_EQ(line.Get_A(), 3.3);
+}
+
+TEST(LineSegment, Can_Set_Initial_B_Value) {
+  LineSegment2D line(3.3, 0, 11);
+  line.Set_B(13.7);
+  ASSERT_DOUBLE_EQ(line.Get_B(), 13.7);
+}
+
+TEST(LineSegment, Can_Set_Initial_C_Value) {
+  LineSegment2D line(0, 0, 0);
+  line.Set_C(-73.7);
+  ASSERT_DOUBLE_EQ(line.Get_C(), -73.7);
 }
 
 TEST(LineSegment, Set_Initial_Value_Works_Correctly) {
