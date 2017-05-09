edd85ae5225cfc59f9bb9840513d205d4a41a113
Umlore1998 <iv.kiselev1998@yandex.ru>
2017-05-09 10:49:43 +0300

    - All TAB

--- modules/ivan-kiselev-polinom_calc/include/TDatList.h
+++ modules/ivan-kiselev-polinom_calc/include/TDatList.h
@@ -1,45 +1,41 @@
 //  Copyright 2017 Ivan Kiselev
-
-#ifndef _TDatList_
-#define _TDatList_
-
+#ifndef MODULES_IVAN_KISELEV_POLINOM_CALC_INCLUDE_TDATLIST_H_
+#define MODULES_IVAN_KISELEV_POLINOM_CALC_INCLUDE_TDATLIST_H_
 #include "TDatLink.h"
-
 class TDatList{
-protected:
-	PTDatLink pFirst;
-	PTDatLink pLast;
-	PTDatLink pCurrLink;
-	PTDatLink pPrevLink;
-	PTDatLink pStop;
-	int CurrPos;
-	int ListLen;
-protected:
-	PTDatLink GetLink(PTDatValue pVal = NULL, PTDatLink pLink = NULL)
-	{
-		return new TDatLink(pVal, pLink);
-	}
-public:
-	TDatList();
-	~TDatList() {
-		DelList();
-	};
-	PTDatValue GetDatValue();
-	virtual int IsEmpty()  const
-	{
-		return pFirst == pLast;
-	}
-	int GetListLength()    const { return ListLen; };
-	virtual void Reset(void);
-	virtual bool IsListEnded(void) const;
-	int GoNext(void);
-	virtual void InsFirst(PTDatValue pVal = NULL);
-	virtual void InsLast(PTDatValue pVal = NULL);
-	virtual void InsCurrent(PTDatValue pVal = NULL);
-	virtual void DelFirst(void);
-	virtual void DelCurrent(void);
-	virtual void DelList(void);
+ protected:
+PTDatLink pFirst;
+PTDatLink pLast;
+PTDatLink pCurrLink;
+PTDatLink pPrevLink;
+PTDatLink pStop;
+int CurrPos;
+int ListLen;
+ protected:
+PTDatLink GetLink(PTDatValue pVal = NULL, PTDatLink pLink = NULL)
+{
+return new TDatLink(pVal, pLink);
+}
+ public:
+TDatList();
+~TDatList() {
+DelList();
 };
-
-#endif
+PTDatValue GetDatValue();
+virtual int IsEmpty()  const
+{
+return pFirst == pLast;
+}
+int GetListLength()    const { return ListLen; };
+virtual void Reset(void);
+virtual bool IsListEnded(void) const;
+int GoNext(void);
+virtual void InsFirst(PTDatValue pVal = NULL);
+virtual void InsLast(PTDatValue pVal = NULL);
+virtual void InsCurrent(PTDatValue pVal = NULL);
+virtual void DelFirst(void);
+virtual void DelCurrent(void);
+virtual void DelList(void);
+};
+#endif  //  MODULES_IVAN_KISELEV_POLINOM_CALC_INCLUDE_TDATLIST_H_
 
--- modules/ivan-kiselev-polinom_calc/include/TMonom.h
+++ modules/ivan-kiselev-polinom_calc/include/TMonom.h
@@ -1,45 +1,40 @@
-// Copyright 2017 Ivan Kiselev
-
-#ifndef _TMonom_
-#define _TMonon_
-
+//  Copyright 2017 Ivan Kiselev
+#ifndef MODULES_IVAN_KISELEV_POLINOM_CALC_INCLUDE_TMONOM_H_
+#define MODULES_IVAN_KISELEV_POLINOM_CALC_INCLUDE_TMONOM_H_
 #include "TDatValue.h"
 #include <iostream>
-
 using namespace std;
-
 class TMonom : public TDatValue{
-protected:
-	int Coeff; // monom's coeff
-	int XInd; // index X
-	int YInd; // index Y
-	int ZInd; // index Z
-public:
-	TMonom(int cval = 1, int x = 0, int y = 0, int z = 0)
-	{
-		Coeff = cval;
-		XInd = x;
-		YInd = y;
-		ZInd = z;
-	};
-	~TMonom() {};
-	virtual TDatValue * GetCopy();
-	void SetCoeff(int cval);
-	int  GetCoeff(void);
-	void SetIndexX(int ival);
-	void SetIndexY(int ival);
-	void SetIndexZ(int ival);
-	int  GetIndexX(void);
-	int  GetIndexY(void);
-	int  GetIndexZ(void);
-	TMonom& operator=(const TMonom &tm);
-	TMonom operator*(TMonom &mon);
-	bool operator==(const TMonom &tm);
-	bool EqualityExponent(TMonom &tm);
-	bool operator<(TMonom &tm); // prioritet x > y > z
-	bool operator>(TMonom &tm); // prioritet x > y > z
-	friend ostream& operator<<(ostream &os, TMonom * tm);
-	friend class TPolinom;
+ protected:
+int Coeff;  //  monom's coeff
+int XInd;  //  index X
+int YInd;  //  index Y
+int ZInd;  //  index Z
+ public:
+TMonom(int cval = 1, int x = 0, int y = 0, int z = 0)
+{
+Coeff = cval;
+XInd = x;
+YInd = y;
+ZInd = z;
 };
-
-#endif
+~TMonom() {};
+virtual TDatValue * GetCopy();
+void SetCoeff(int cval);
+int  GetCoeff(void);
+void SetIndexX(int ival);
+void SetIndexY(int ival);
+void SetIndexZ(int ival);
+int GetIndexX(void);
+int GetIndexY(void);
+int GetIndexZ(void);
+TMonom& operator=(const TMonom &tm);
+TMonom operator*(TMonom &mon);
+bool operator==(const TMonom &tm);
+bool EqualityExponent(TMonom &tm);
+bool operator<(TMonom &tm);  //  prioritet x > y > z
+bool operator>(TMonom &tm);  //  prioritet x > y > z
+friend ostream& operator<<(ostream &os, TMonom * tm);
+friend class TPolinom;
+};
+#endif  //  MODULES_IVAN_KISELEV_POLINOM_CALC_INCLUDE_TMONOM_H_
