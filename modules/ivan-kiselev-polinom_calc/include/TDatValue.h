957454d8bc514de2b5a4f2c287016f9dd5edac33
Umlore1998 <iv.kiselev1998@yandex.ru>
2017-05-09 07:56:06 +0300

    3

--- modules/ivan-kiselev-polinom_calc/include/TDatValue.h
+++ modules/ivan-kiselev-polinom_calc/include/TDatValue.h
@@ -5,8 +5,8 @@
 
 class TDatValue {
 public:
-virtual TDatValue * GetCopy() = 0; // copia
-~TDatValue() {};
+ virtual TDatValue * GetCopy() = 0; // copia
+ ~TDatValue() {};
 };
 
 #endif // MODULES_IVAN_KISELEV_POLINOM_CALC_INCLUDE_TDATVALUE_H_
--- modules/ivan-kiselev-polinom_calc/include/TRootLink.h
+++ modules/ivan-kiselev-polinom_calc/include/TRootLink.h
@@ -1,39 +1,40 @@
 // Copyright 2017 Ivan Kiselev
 
-#ifndef _TRootLink_
-#define _TRootLink_
+#ifndef MODULES_IVAN_KISELEV_POLINOM_CALC_INCLUDE_TROOTLINK_H_
+#define MODULES_IVAN_KISELEV_POLINOM_CALC_INCLUDE_TROOTLINK_H_
 
 #include "TDatValue.h"
+#include <iostream>
 
 class TRootLink;
 typedef TRootLink *PTRootLink;
 typedef TDatValue *PTDatValue;
 class TRootLink {
 protected:
-	PTRootLink pNext; // next point
+ PTRootLink pNext; // next point
 public:
-	TRootLink(PTRootLink pN = NULL)
-	{
-		pNext = pN;
-	}
-
-	~TRootLink() { };
-	PTRootLink GetNextLink()
-	{
-		return pNext;
-	}
-
-	void SetNextLink(PTRootLink pLink) {
-		pNext = pLink;
-	}
-	void InsNextLink(PTRootLink pLink)
-	{
-		PTRootLink p = pNext;
-		pNext = pLink;
-		if (pLink != NULL) pLink->pNext = p;
-	}
-	virtual void SetDatValue(PTDatValue pVal) = 0;
-	virtual PTDatValue GetDatValue() = 0;
-	friend class TDatList;
+ TRootLink(PTRootLink pN = NULL)
+ {
+ pNext = pN;
+ }
+ ~TRootLink() { };
+ PTRootLink GetNextLink()
+ {
+ return pNext;
+ }
+ void SetNextLink(PTRootLink pLink)
+ {
+ pNext = pLink;
+ }
+ void InsNextLink(PTRootLink pLink)
+ {
+ PTRootLink p = pNext;
+ pNext = pLink;
+ if (pLink != NULL) pLink->pNext = p;
+ }
+ virtual void SetDatValue(PTDatValue pVal) = 0;
+ virtual PTDatValue GetDatValue() = 0;
+ friend class TDatList;
 };
-#endif
+
+#endif // MODULES_IVAN_KISELEV_POLINOM_CALC_INCLUDE_TROOTLINK_H_
