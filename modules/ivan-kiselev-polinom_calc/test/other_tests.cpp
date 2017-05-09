6f89131d0fadff68c242bfd743efd630f452555d
Umlore1998 <iv.kiselev1998@yandex.ru>
2017-05-09 17:04:01 +0300

    It's All?

--- modules/ivan-kiselev-polinom_calc/test/other_tests.cpp
+++ modules/ivan-kiselev-polinom_calc/test/other_tests.cpp
@@ -1,10 +1,7 @@
 // Copyright 2017 Ivan Kiselev
-
 #include "../3rdparty/gtest/gtest.h"
 #include "../include/TPolinoms.h"
-
-TEST(TRootLink, Set_Next_Link)
-{
+TEST(TRootLink, Set_Next_Link) {
 TDatLink sec(NULL);
 PTDatLink pdv1 = &sec;
 TDatLink first(NULL);
@@ -14,8 +11,7 @@ first.SetNextLink(pdv1);
 EXPECT_EQ(&sec, first.GetNextLink());
 }
 
-TEST(TRootLink, Get_Next_Link)
-{
+TEST(TRootLink, Get_Next_Link) {
 TDatLink sec(NULL);
 PTDatLink pdv1 = &sec;
 TDatLink first(NULL, pdv1);
@@ -23,8 +19,7 @@ TDatLink first(NULL, pdv1);
 EXPECT_EQ(&sec, first.GetNextLink());
 }
 
-TEST(TRootLink, Ins_Next_Link)
-{
+TEST(TRootLink, Ins_Next_Link) {
 TDatLink sec(NULL);
 PTDatLink pdv1 = &sec;
 TDatLink first(NULL, pdv1);
@@ -36,23 +31,20 @@ EXPECT_EQ(&third, first.GetNextLink());
 }
 
 
-TEST(TDatLink, create_DatLink)
-{
+TEST(TDatLink, create_DatLink) {
 TDatLink sec(NULL);
 
 EXPECT_NO_FATAL_FAILURE(TDatLink first(NULL, &sec));
 }
 
-TEST(TDatLink, Set_DatLalue)
-{
+TEST(TDatLink, Set_DatLalue) {
 PTDatValue pdv = NULL;
 TDatLink dl(NULL, NULL);
 
 EXPECT_NO_FATAL_FAILURE(dl.SetDatValue(pdv));
 }
 
-TEST(TDatLink, Get_DatValue)
-{
+TEST(TDatLink, Get_DatValue) {
 PTDatValue pdv = NULL;
 TDatLink dl;
 
@@ -61,8 +53,7 @@ dl.SetDatValue(pdv);
 EXPECT_EQ(dl.GetDatValue(), pdv);
 }
 
-TEST(TDatLink, Get_Next_DatLink)
-{
+TEST(TDatLink, Get_Next_DatLink) {
 TDatLink dl1;
 TDatLink dl2;
 
@@ -71,80 +62,70 @@ dl1.SetNextLink(&dl2);
 EXPECT_EQ(&dl2, dl1.GetNextDatLink());
 }
 
-TEST(TMonom, Equal)
-{
+TEST(TMonom, Equal) {
 TMonom first(10, 20);
 TMonom second(10, 20);
 
 EXPECT_TRUE(first == second);
 }
 
-TEST(TMonom, Not_equal)
-{
+TEST(TMonom, Not_equal) {
 TMonom first(10, 20);
 TMonom second(10, 25);
 
 EXPECT_TRUE(!(first == second));
 }
 
-TEST(TMonom, EqualityExponent_1)
-{
+TEST(TMonom, EqualityExponent_1) {
 TMonom* first = new TMonom(5, 1, 4, 3);
 TMonom* second = new TMonom(7, 1, 4, 3);
 
 EXPECT_TRUE(first->EqualityExponent(*second));
 }
 
-TEST(TMonom, EqualityExponent_2)
-{
+TEST(TMonom, EqualityExponent_2) {
 TMonom* first = new TMonom(5, 1, 4, 3);
 TMonom* second = new TMonom(7, 1, 3, 4);
 
 EXPECT_FALSE(first->EqualityExponent(*second));
 }
 
-TEST(TMonom, Comparison_of_monomials_1)
-{
+TEST(TMonom, Comparison_of_monomials_1) {
 TMonom* first = new TMonom(5, 1, 4, 3);
 TMonom* second = new TMonom(7, 1, 3, 4);
 
 EXPECT_TRUE(*second < *first);
 }
 
-TEST(TMonom, Comparison_of_monomials_2)
-{
+TEST(TMonom, Comparison_of_monomials_2) {
 TMonom* first = new TMonom(5, 1, 4, 3);
 TMonom* second = new TMonom(7, 1, 3, 4);
 
 EXPECT_TRUE(*second < *first);
 }
 
-TEST(TMonom, Comparison_of_monomials_3)
-{
+TEST(TMonom, Comparison_of_monomials_3) {
 TMonom* first = new TMonom(5, 1, 3, 4);
 TMonom* second = new TMonom(7, 1, 3, 4);
 
 EXPECT_FALSE(*second < *first);
 }
 
-TEST(TMonom, Comparison_of_monomials_4)
-{
+TEST(TMonom, Comparison_of_monomials_4) {
 TMonom* first = new TMonom(1, 0, 0, 1);
 TMonom* second = new TMonom(5, 0, 0, 2);
 
 EXPECT_FALSE(*second < *first);
 }
 
-TEST(TMonom, Comparison_of_monomials_5)
-{
+TEST(TMonom, Comparison_of_monomials_5) {
 TMonom* first = new TMonom(5, 1, 4, 3);
 TMonom* second = new TMonom(7, 0, 6, 4);
 
 EXPECT_FALSE(*first < *second);
 }
 
-TEST(TMonom, multiplication_Monom)
-{
+TEST(TMonom, multiplication_Monom) {
 TMonom first(3, 1, 3, 5);
 TMonom second(4, 0, -3, 1);
 TMonom result(12, 1, 0, 6);
@@ -154,8 +135,7 @@ first = first * second;
 EXPECT_TRUE(result == first);
 }
 
-TEST(TPolinom, Auto_Equal)
-{
+TEST(TPolinom, Auto_Equal) {
 int size = 1;
 int mon[][4] = { { 1, 0, 0, 3 } };
 TPolinom old(mon, size);
@@ -163,8 +143,7 @@ TPolinom old(mon, size);
 EXPECT_TRUE(old == old);
 }
 
-TEST(TPolinom, can_compare_the_polynoms)
-{
+TEST(TPolinom, can_compare_the_polynoms) {
 int size = 3;
 int mon[][4] = { { 2, 1, 0, 0 } , { 2, 0, 0, 4 } , { 1, 0, 0, 3 } };
 TPolinom Pol1(mon, size);
@@ -173,8 +152,7 @@ TPolinom Pol2(mon, size);
 EXPECT_TRUE(Pol1 == Pol2);
 }
 
-TEST(TPolinom, can_copy_polinoms)
-{
+TEST(TPolinom, can_copy_polinoms) {
 const int size = 2;
 int mon[][4] = { { 2, 0, 0, 4 } , { 5, 0, 0, 3 } };
 TPolinom Pol1(mon, size);
@@ -183,8 +161,7 @@ TPolinom Pol2(Pol1);
 EXPECT_TRUE(Pol1 == Pol2);
 }
 
-TEST(TPolinom, can_assign_polynoms)
-{
+TEST(TPolinom, can_assign_polynoms) {
 const int size = 2;
 int mon[][4] = { { 2, 0, 0, 4 } , { 5, 0, 0, 3 } };
 TPolinom Pol1(mon, size);
@@ -195,8 +172,7 @@ Pol2 = Pol1;
 EXPECT_TRUE(Pol1 == Pol2);
 }
 
-TEST(TPolinom, can_assign_empty_polynom)
-{
+TEST(TPolinom, can_assign_empty_polynom) {
 TPolinom Pol1;
 TPolinom Pol2;
 
@@ -205,8 +181,7 @@ Pol2 = Pol1;
 EXPECT_TRUE(Pol1 == Pol2);
 }
 
-TEST(TPolinom, can_add_Monom_1)
-{
+TEST(TPolinom, can_add_Monom_1) {
 const int Size = 2;
 const int resSize = 3;
 int Mon[][4] = { { 2, 0, 0, 4 } , { 5, 0, 0, 3 } };
@@ -220,8 +195,7 @@ Pol.AddMonom(&monom);
 EXPECT_TRUE(res == Pol);
 }
 
-TEST(TPolinom, can_add_Monom_2)
-{
+TEST(TPolinom, can_add_Monom_2) {
 const int Size = 2;
 const int resSize = 1;
 int Mon[][4] = { { 2, 0, 0, 4 } , { 5, 0, 0, 3 } };
@@ -235,8 +209,7 @@ Pol.AddMonom(&monom);
 EXPECT_TRUE(res == Pol);
 }
 
-TEST(TPolinom, can_add_Monom_3)
-{
+TEST(TPolinom, can_add_Monom_3) {
 const int Size = 2;
 const int resSize = 2;
 int Mon[][4] = { { 2, 0, 0, 4 } , { 5, 0, 0, 3 } };
@@ -250,8 +223,7 @@ Pol.AddMonom(&monom);
 EXPECT_TRUE(res == Pol);
 }
 
-TEST(TPolinom, can_add_Monom_4)
-{
+TEST(TPolinom, can_add_Monom_4) {
 const int Size = 2;
 const int resSize = 3;
 int Mon[][4] = { { 2, 0, 0, 5 } , { 1, 0, 0, 4 } , { 1, 0, 0, 1 } };
@@ -265,8 +237,7 @@ Pol.AddMonom(&monom);
 EXPECT_TRUE(res == Pol);
 }
 
-TEST(TPolinom, can_subtract_Monom_1)
-{
+TEST(TPolinom, can_subtract_Monom_1) {
 const int Size = 2;
 const int resSize = 2;
 int Mon[][4] = { { 4, 0, 0, 5 } , { 5, 0, 0, 4 } };
@@ -280,8 +251,7 @@ Pol.SubMonom(&monom);
 EXPECT_TRUE(res == Pol);
 }
 
-TEST(TPolinom, can_subtract_Monom_2)
-{
+TEST(TPolinom, can_subtract_Monom_2) {
 const int Size = 2;
 const int resSize = 2;
 int Mon[][4] = { { 4, 0, 0, 5 } , { 3, 0, 0, 4 } };
@@ -295,8 +265,7 @@ Pol.SubMonom(&monom);
 EXPECT_TRUE(res == Pol);
 }
 
-TEST(TPolinom, can_subtract_Monom_3)
-{
+TEST(TPolinom, can_subtract_Monom_3) {
 const int Size = 3;
 const int resSize = 2;
 int Mon[][4] = { { 4, 0, 0, 5 } , { 3, 0, 0, 4 } , {2 , 0 , 0 , 2} };
@@ -310,8 +279,7 @@ Pol.SubMonom(&monom);
 EXPECT_TRUE(res == Pol);
 }
 
-TEST(TPolinom, can_add_up_linear_polynoms)
-{
+TEST(TPolinom, can_add_up_linear_polynoms) {
 const int size = 1;
 const int expected_size = 1;
 int mon1[][4] = { { 2, 0, 0, 1 } };
@@ -326,8 +294,7 @@ TPolinom Pol = Pol1 + Pol2;
 EXPECT_TRUE(Pol == expected_Pol);
 }
 
-TEST(TPolinom, can_add_up_simple_polynoms_A_plus_B)
-{
+TEST(TPolinom, can_add_up_simple_polynoms_A_plus_B) {
 const int size1 = 3;
 const int size2 = 4;
 const int expected_size = 4;
@@ -343,8 +310,7 @@ TPolinom Pol = Pol1 + Pol2;
 EXPECT_TRUE(expected_Pol == Pol);
 }
 
-TEST(TPolinom, can_add_up_simple_polynoms_B_plus_A)
-{
+TEST(TPolinom, can_add_up_simple_polynoms_B_plus_A) {
 const int size1 = 3;
 const int size2 = 4;
 const int expected_size = 4;
@@ -360,8 +326,7 @@ TPolinom Pol = Pol2 + Pol1;
 EXPECT_TRUE(expected_Pol == Pol);
 }
 
-TEST(TPolinom, can_add_up_polynoms)
-{
+TEST(TPolinom, can_add_up_polynoms) {
 const int size1 = 5;
 const int size2 = 4;
 const int expected_size = 6;
@@ -377,8 +342,7 @@ TPolinom Pol = Pol1 + Pol2;
 EXPECT_TRUE(Pol == expected_Pol);
 }
 
-TEST(TPolinom, can_add_up_many_polynoms_1)
-{
+TEST(TPolinom, can_add_up_many_polynoms_1) {
 const int size1 = 3;
 const int size2 = 4;
 const int size3 = 3;
@@ -398,8 +362,7 @@ Pol = Pol1 + Pol2 + Pol3;
 EXPECT_TRUE(Pol == expected_Pol);
 }
 
-TEST(TPolinom, can_add_up_many_polynoms_2)
-{
+TEST(TPolinom, can_add_up_many_polynoms_2) {
 const int size1 = 3;
 const int size2 = 4;
 const int size3 = 3;
@@ -419,8 +382,7 @@ Pol = Pol2 + Pol3 + Pol1;
 EXPECT_TRUE(Pol == expected_Pol);
 }
 
-TEST(TPolinom, can_add_up_many_polynoms_3)
-{
+TEST(TPolinom, can_add_up_many_polynoms_3) {
 const int size1 = 3;
 const int size2 = 4;
 const int size3 = 3;
@@ -440,8 +402,7 @@ Pol = Pol2 + Pol1 + Pol3;
 EXPECT_TRUE(Pol == expected_Pol);
 }
 
-TEST(TPolinom, can_subtract_simple_polinom)
-{
+TEST(TPolinom, can_subtract_simple_polinom) {
 int size1 = 3;
 int size2 = 1;
 int res_size = 3;
@@ -459,8 +420,7 @@ EXPECT_TRUE(res == first);
 }
 
 
-TEST(TPolinom, can_subtract_polinom)
-{
+TEST(TPolinom, can_subtract_polinom) {
 int size1 = 3;
 int res_size = 2;
 int mon1[][4] = { { 9, 0, 0, 4 } , { 8, 0, 0, 3 } , { 5, 0, 0, 2 } };
@@ -475,8 +435,7 @@ first = first - second;
 EXPECT_TRUE(res == first);
 }
 
-TEST(TPolinom, can_subtract_up_simple_polynoms_A_minus_B)
-{
+TEST(TPolinom, can_subtract_up_simple_polynoms_A_minus_B) {
 const int size1 = 3;
 const int size2 = 4;
 const int expected_size = 4;
@@ -492,8 +451,7 @@ TPolinom Pol = Pol1 - Pol2;
 EXPECT_TRUE(expected_Pol == Pol);
 }
 
-TEST(TPolinom, can_subtract_up_simple_polynoms_B_minus_A)
-{
+TEST(TPolinom, can_subtract_up_simple_polynoms_B_minus_A) {
 const int size1 = 3;
 const int size2 = 4;
 const int expected_size = 4;
@@ -509,8 +467,7 @@ TPolinom Pol = Pol2 - Pol1;
 EXPECT_TRUE(expected_Pol == Pol);
 }
 
-TEST(TPolinom, can_subtract_up_polynoms)
-{
+TEST(TPolinom, can_subtract_up_polynoms) {
 const int size1 = 5;
 const int size2 = 4;
 const int expected_size = 6;
@@ -529,8 +486,7 @@ TPolinom Pol = Pol1 - Pol2;
 EXPECT_TRUE(Pol == expected_Pol);
 }
 
-TEST(TPolinom, multiplication_with_simple_polinom)
-{
+TEST(TPolinom, multiplication_with_simple_polinom) {
 int size1 = 4;
 int size2 = 1;
 int mon1[][4] = { { 5, 3, 0, 6 } , { 4, 2, 4, 3 } , { 10, 2, 3, 5 } , { 10, 0, 0, 0 } };
@@ -544,8 +500,7 @@ TPolinom result(resMon, size1);
 EXPECT_TRUE(result == (first * second));
 }
 
-TEST(TPolinom, multiplication_with_big_polinom_4x4)
-{
+TEST(TPolinom, multiplication_with_big_polinom_4x4) {
 int size = 4;
 int res_size = 16;
 int mon1[][4] = { { 5, 3, 0, 6 } , { 5, 2, 4, 3 } , { 10, 2, 3, 5 } , { 10, 0, 0, 0 } };
