ee8742a47f5a0ae9119877cecabd1cae517cb3d4
Splav555 <olegka35@gmail.com>
2017-05-15 10:32:37 +0300

    start

---
+++ modules/triangle/application/triangle.cpp
@@ -0,0 +1,11 @@
+// Copyright 2017 Tarasov Oleg
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+int main(int argc, const char** argv)
+{
+    printf("Hello World\n");
+    return 0;
+}
