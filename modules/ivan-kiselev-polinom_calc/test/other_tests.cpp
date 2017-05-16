c238c4cbcdc98c27a52b766bd46d216915047d24
Umlore1998 <iv.kiselev1998@yandex.ru>
2017-05-16 16:01:33 +0300

    ,_{

--- modules/ivan-kiselev-polinom_calc/test/other_tests.cpp
+++ modules/ivan-kiselev-polinom_calc/test/other_tests.cpp
@@ -202,7 +202,7 @@ TEST(THeadRing, Ins_First) {
     int size = 1;
     int res_size = 2;
     int mon[][4] = { { 1, 0, 0, 3 } };
-    int resmon[][4] = { { 2, 1, 0, 0 } ,{ 1, 0, 0, 3 } };
+    int resmon[][4] = { { 2, 1, 0, 0 } , { 1, 0, 0, 3 } };
     TMonom Mon(2, 1, 0, 0);
     TPolinom old(mon, size);
     TPolinom res(resmon, res_size);
@@ -214,7 +214,7 @@ TEST(THeadRing, Ins_First) {
 TEST(THeadRing, Del_First) {
     int size = 2;
     int res_size = 1;
-    int mon[][4] = { { 2, 1, 0, 0 } ,{ 1, 0, 0, 3 } };
+    int mon[][4] = { { 2, 1, 0, 0 } , { 1, 0, 0, 3 } };
     int resmon[][4] = { { 1, 0, 0, 3 } };
     TPolinom old(mon, size);
     TPolinom res(resmon, res_size);
@@ -233,7 +233,7 @@ TEST(TPolinom, Auto_Equal) {
 
 TEST(TPolinom, can_compare_the_polynoms) {
     int size = 3;
-    int mon[][4] = { { 2, 1, 0, 0 } ,{ 2, 0, 0, 4 } ,{ 1, 0, 0, 3 } };
+    int mon[][4] = { { 2, 1, 0, 0 } , { 2, 0, 0, 4 } , { 1, 0, 0, 3 } };
     TPolinom Pol1(mon, size);
     TPolinom Pol2(mon, size);
 
@@ -242,7 +242,7 @@ TEST(TPolinom, can_compare_the_polynoms) {
 
 TEST(TPolinom, can_copy_polinoms) {
     const int size = 2;
-    int mon[][4] = { { 2, 0, 0, 4 } ,{ 5, 0, 0, 3 } };
+    int mon[][4] = { { 2, 0, 0, 4 } , { 5, 0, 0, 3 } };
     TPolinom Pol1(mon, size);
     TPolinom Pol2(Pol1);
 
@@ -251,7 +251,7 @@ TEST(TPolinom, can_copy_polinoms) {
 
 TEST(TPolinom, can_assign_polynoms) {
     const int size = 2;
-    int mon[][4] = { { 2, 0, 0, 4 } ,{ 5, 0, 0, 3 } };
+    int mon[][4] = { { 2, 0, 0, 4 } , { 5, 0, 0, 3 } };
     TPolinom Pol1(mon, size);
     TPolinom Pol2;
 
@@ -272,8 +272,8 @@ TEST(TPolinom, can_assign_empty_polynom) {
 TEST(TPolinom, can_add_Monom_1) {
     const int Size = 2;
     const int resSize = 3;
-    int Mon[][4] = { { 2, 0, 0, 4 } ,{ 5, 0, 0, 3 } };
-    int resMon[][4] = { { 2, 0, 0, 4 } ,{ 5, 0, 0, 3 } ,{ 10, 0, 0, 2 } };
+    int Mon[][4] = { { 2, 0, 0, 4 } , { 5, 0, 0, 3 } };
+    int resMon[][4] = { { 2, 0, 0, 4 } , { 5, 0, 0, 3 } , { 10, 0, 0, 2 } };
     TPolinom Pol(Mon, Size);
     TMonom monom(10, 0, 0, 2);
     TPolinom res(resMon, resSize);
@@ -286,7 +286,7 @@ TEST(TPolinom, can_add_Monom_1) {
 TEST(TPolinom, can_add_Monom_2) {
     const int Size = 2;
     const int resSize = 1;
-    int Mon[][4] = { { 2, 0, 0, 4 } ,{ 5, 0, 0, 3 } };
+    int Mon[][4] = { { 2, 0, 0, 4 } , { 5, 0, 0, 3 } };
     int resMon[][4] = { { 2, 0, 0, 4 } };
     TPolinom Pol(Mon, Size);
     TMonom monom(-5, 0, 0, 3);
@@ -300,8 +300,8 @@ TEST(TPolinom, can_add_Monom_2) {
 TEST(TPolinom, can_add_Monom_3) {
     const int Size = 2;
     const int resSize = 2;
-    int Mon[][4] = { { 2, 0, 0, 4 } ,{ 5, 0, 0, 3 } };
-    int resMon[][4] = { { 6, 0, 0, 4 } ,{ 5, 0, 0, 3 } };
+    int Mon[][4] = { { 2, 0, 0, 4 } , { 5, 0, 0, 3 } };
+    int resMon[][4] = { { 6, 0, 0, 4 } , { 5, 0, 0, 3 } };
     TPolinom Pol(Mon, Size);
     TMonom monom(4, 0, 0, 4);
     TPolinom res(resMon, resSize);
@@ -314,9 +314,9 @@ TEST(TPolinom, can_add_Monom_3) {
 TEST(TPolinom, can_add_Monom_4) {
     const int Size = 2;
     const int resSize = 3;
-    int Mon[][4] = { { 2, 0, 0, 5 } ,{ 1, 0, 0, 4 } ,{ 1, 0, 0, 1 } };
-    int resMon[][4] = { { 2, 0, 0, 5 } ,{ 1, 0, 0, 4 } ,
-    { 5, 0, 0, 2 } ,{ 1, 0, 0, 1 } };
+    int Mon[][4] = { { 2, 0, 0, 5 } , { 1, 0, 0, 4 } , { 1, 0, 0, 1 } };
+    int resMon[][4] = { { 2, 0, 0, 5 } , { 1, 0, 0, 4 } ,
+    { 5, 0, 0, 2 } , { 1, 0, 0, 1 } };
     TPolinom Pol(Mon, Size);
     TMonom monom(5, 0, 0, 2);
     TPolinom res(resMon, resSize);
@@ -329,8 +329,8 @@ TEST(TPolinom, can_add_Monom_4) {
 TEST(TPolinom, can_subtract_Monom_1) {
     const int Size = 2;
     const int resSize = 2;
-    int Mon[][4] = { { 4, 0, 0, 5 } ,{ 5, 0, 0, 4 } };
-    int resMon[][4] = { { 4, 0, 0, 5 } ,{ 2, 0, 0, 4 } };
+    int Mon[][4] = { { 4, 0, 0, 5 } , { 5, 0, 0, 4 } };
+    int resMon[][4] = { { 4, 0, 0, 5 } , { 2, 0, 0, 4 } };
     TPolinom Pol(Mon, Size);
     TMonom monom(3, 0, 0, 4);
     TPolinom res(resMon, resSize);
@@ -343,8 +343,8 @@ TEST(TPolinom, can_subtract_Monom_1) {
 TEST(TPolinom, can_subtract_Monom_2) {
     const int Size = 2;
     const int resSize = 2;
-    int Mon[][4] = { { 4, 0, 0, 5 } ,{ 3, 0, 0, 4 } };
-    int resMon[][4] = { { 1, 0, 0, 5 } ,{ 3, 0, 0, 4 } };
+    int Mon[][4] = { { 4, 0, 0, 5 } , { 3, 0, 0, 4 } };
+    int resMon[][4] = { { 1, 0, 0, 5 } , { 3, 0, 0, 4 } };
     TPolinom Pol(Mon, Size);
     TMonom monom(3, 0, 0, 5);
     TPolinom res(resMon, resSize);
@@ -357,8 +357,8 @@ TEST(TPolinom, can_subtract_Monom_2) {
 TEST(TPolinom, can_subtract_Monom_3) {
     const int Size = 3;
     const int resSize = 2;
-    int Mon[][4] = { { 4, 0, 0, 5 } ,{ 3, 0, 0, 4 } ,{ 2 , 0 , 0 , 2 } };
-    int resMon[][4] = { { 4, 0, 0, 5 } ,{ 2, 0, 0, 2 } };
+    int Mon[][4] = { { 4, 0, 0, 5 } , { 3, 0, 0, 4 } , { 2 , 0 , 0 , 2 } };
+    int resMon[][4] = { { 4, 0, 0, 5 } , { 2, 0, 0, 2 } };
     TPolinom Pol(Mon, Size);
     TMonom monom(3, 0, 0, 4);
     TPolinom res(resMon, resSize);
@@ -387,12 +387,12 @@ TEST(TPolinom, can_add_up_simple_polynoms_A_plus_B) {
     const int size1 = 3;
     const int size2 = 4;
     const int expected_size = 4;
-    int mon1[][4] = { { 9, 0, 0, 4 } ,{ 8, 0, 0, 3 } ,
+    int mon1[][4] = { { 9, 0, 0, 4 } , { 8, 0, 0, 3 } ,
     { 5, 0, 0, 2 } };
-    int mon2[][4] = { { 2, 0, 0, 5 } ,{ 1, 0, 0, 4 } ,
-    { -8, 0, 0, 3 } ,{ 1, 0, 0, 1 } };
-    int expected_mon[][4] = { { 2, 0, 0, 5 } ,{ 10, 0, 0, 4 } ,
-    { 5, 0, 0, 2 } ,{ 1, 0, 0, 1 } };
+    int mon2[][4] = { { 2, 0, 0, 5 } , { 1, 0, 0, 4 } ,
+    { -8, 0, 0, 3 } , { 1, 0, 0, 1 } };
+    int expected_mon[][4] = { { 2, 0, 0, 5 } , { 10, 0, 0, 4 } ,
+    { 5, 0, 0, 2 } , { 1, 0, 0, 1 } };
     TPolinom Pol1(mon1, size1);
     TPolinom Pol2(mon2, size2);
     TPolinom expected_Pol(expected_mon, expected_size);
@@ -406,12 +406,12 @@ TEST(TPolinom, can_add_up_simple_polynoms_B_plus_A) {
     const int size1 = 3;
     const int size2 = 4;
     const int expected_size = 4;
-    int mon1[][4] = { { 9, 0, 0, 4 } ,{ 8, 0, 0, 3 } ,
+    int mon1[][4] = { { 9, 0, 0, 4 } , { 8, 0, 0, 3 } ,
     { 5, 0, 0, 2 } };
-    int mon2[][4] = { { 2, 0, 0, 5 } ,{ 1, 0, 0, 4 } ,
-    { -8, 0, 0, 3 } ,{ 1, 0, 0, 1 } };
-    int expected_mon[][4] = { { 2, 0, 0, 5 } ,{ 10, 0, 0, 4 } ,
-    { 5, 0, 0, 2 } ,{ 1, 0, 0, 1 } };
+    int mon2[][4] = { { 2, 0, 0, 5 } , { 1, 0, 0, 4 } ,
+    { -8, 0, 0, 3 } , { 1, 0, 0, 1 } };
+    int expected_mon[][4] = { { 2, 0, 0, 5 } , { 10, 0, 0, 4 } ,
+    { 5, 0, 0, 2 } , { 1, 0, 0, 1 } };
     TPolinom Pol1(mon1, size1);
     TPolinom Pol2(mon2, size2);
     TPolinom expected_Pol(expected_mon, expected_size);
@@ -425,12 +425,12 @@ TEST(TPolinom, can_add_up_polynoms) {
     const int size1 = 5;
     const int size2 = 4;
     const int expected_size = 6;
-    int mon1[][4] = { { 10, 9, 9, 9 } ,{ -21, 5, 0, 0 } ,
-    { 10, 4, 3, 2 } ,{ 8, 3, 2, 1 } ,{ 5, 2, 1, 3 } };
-    int mon2[][4] = { { 20, 7, 0, 2 } ,{ 1, 5, 0, 0 } ,
-    { -8, 3, 2, 1 } ,{ 15, 0 } };
-    int expected_mon[][4] = { { 10, 9, 9, 9 } ,{ 20, 7, 0, 2 } ,
-    { -20, 5, 0, 0 } ,{ 10, 4, 3, 2 } ,{ 5, 2, 1, 3 } ,
+    int mon1[][4] = { { 10, 9, 9, 9 } , { -21, 5, 0, 0 } ,
+    { 10, 4, 3, 2 } , { 8, 3, 2, 1 } , { 5, 2, 1, 3 } };
+    int mon2[][4] = { { 20, 7, 0, 2 } , { 1, 5, 0, 0 } ,
+    { -8, 3, 2, 1 } , { 15, 0 } };
+    int expected_mon[][4] = { { 10, 9, 9, 9 } , { 20, 7, 0, 2 } ,
+    { -20, 5, 0, 0 } , { 10, 4, 3, 2 } , { 5, 2, 1, 3 } ,
     { 15, 0, 0, 0 } };
     TPolinom Pol1(mon1, size1);
     TPolinom Pol2(mon2, size2);
@@ -446,14 +446,14 @@ TEST(TPolinom, can_add_up_many_polynoms_1) {
     const int size2 = 4;
     const int size3 = 3;
     const int expected_size = 6;
-    int mon1[][4] = { { 9, 0, 0, 4 } ,{ 8, 0, 0, 3 } ,
+    int mon1[][4] = { { 9, 0, 0, 4 } , { 8, 0, 0, 3 } ,
     { 5, 0, 0, 2 } };
-    int mon2[][4] = { { 2, 0, 0, 5 } ,{ 1, 0, 0, 4 } ,
-    { -8, 0, 0, 3 } ,{ 1, 0, 0, 1 } };
-    int mon3[][4] = { { 8, 0, 0, 5 } ,{ 2, 0, 0, 3 } ,
+    int mon2[][4] = { { 2, 0, 0, 5 } , { 1, 0, 0, 4 } ,
+    { -8, 0, 0, 3 } , { 1, 0, 0, 1 } };
+    int mon3[][4] = { { 8, 0, 0, 5 } , { 2, 0, 0, 3 } ,
     { 10, 0, 0, 0 } };
-    int expected_mon[][4] = { { 10, 0, 0, 5 } ,{ 10, 0, 0, 4 } ,
-    { 2, 0, 0, 3 } ,{ 5, 0, 0, 2 } ,{ 1, 0, 0, 1 } ,{ 10, 0, 0, 0 } };
+    int expected_mon[][4] = { { 10, 0, 0, 5 } , { 10, 0, 0, 4 } ,
+    { 2, 0, 0, 3 } , { 5, 0, 0, 2 } , { 1, 0, 0, 1 } , { 10, 0, 0, 0 } };
     TPolinom Pol1(mon1, size1);
     TPolinom Pol2(mon2, size2);
     TPolinom Pol3(mon3, size3);
@@ -470,14 +470,14 @@ TEST(TPolinom, can_add_up_many_polynoms_2) {
     const int size2 = 4;
     const int size3 = 3;
     const int expected_size = 6;
-    int mon1[][4] = { { 9, 0, 0, 4 } ,{ 8, 0, 0, 3 } ,
+    int mon1[][4] = { { 9, 0, 0, 4 } , { 8, 0, 0, 3 } ,
     { 5, 0, 0, 2 } };
-    int mon2[][4] = { { 2, 0, 0, 5 } ,{ 1, 0, 0, 4 } ,
-    { -8, 0, 0, 3 } ,{ 1, 0, 0, 1 } };
-    int mon3[][4] = { { 8, 0, 0, 5 } ,{ 2, 0, 0, 3 } ,
+    int mon2[][4] = { { 2, 0, 0, 5 } , { 1, 0, 0, 4 } ,
+    { -8, 0, 0, 3 } , { 1, 0, 0, 1 } };
+    int mon3[][4] = { { 8, 0, 0, 5 } , { 2, 0, 0, 3 } ,
     { 10, 0, 0, 0 } };
-    int expected_mon[][4] = { { 10, 0, 0, 5 } ,{ 10, 0, 0, 4 } ,
-    { 2, 0, 0, 3 } ,{ 5, 0, 0, 2 } ,{ 1, 0, 0, 1 } ,{ 10, 0, 0, 0 } };
+    int expected_mon[][4] = { { 10, 0, 0, 5 } , { 10, 0, 0, 4 } ,
+    { 2, 0, 0, 3 } , { 5, 0, 0, 2 } , { 1, 0, 0, 1 } , { 10, 0, 0, 0 } };
     TPolinom Pol1(mon1, size1);
     TPolinom Pol2(mon2, size2);
     TPolinom Pol3(mon3, size3);
@@ -494,14 +494,14 @@ TEST(TPolinom, can_add_up_many_polynoms_3) {
     const int size2 = 4;
     const int size3 = 3;
     const int expected_size = 6;
-    int mon1[][4] = { { 9, 0, 0, 4 } ,{ 8, 0, 0, 3 } ,
+    int mon1[][4] = { { 9, 0, 0, 4 } , { 8, 0, 0, 3 } ,
     { 5, 0, 0, 2 } };
-    int mon2[][4] = { { 2, 0, 0, 5 } ,{ 1, 0, 0, 4 } ,
-    { -8, 0, 0, 3 } ,{ 1, 0, 0, 1 } };
-    int mon3[][4] = { { 8, 0, 0, 5 } ,{ 2, 0, 0, 3 } ,
+    int mon2[][4] = { { 2, 0, 0, 5 } , { 1, 0, 0, 4 } ,
+    { -8, 0, 0, 3 } , { 1, 0, 0, 1 } };
+    int mon3[][4] = { { 8, 0, 0, 5 } , { 2, 0, 0, 3 } ,
     { 10, 0, 0, 0 } };
-    int expected_mon[][4] = { { 10, 0, 0, 5 } ,{ 10, 0, 0, 4 } ,
-    { 2, 0, 0, 3 } ,{ 5, 0, 0, 2 } ,{ 1, 0, 0, 1 } ,{ 10, 0, 0, 0 } };
+    int expected_mon[][4] = { { 10, 0, 0, 5 } , { 10, 0, 0, 4 } ,
+    { 2, 0, 0, 3 } , { 5, 0, 0, 2 } , { 1, 0, 0, 1 } , { 10, 0, 0, 0 } };
     TPolinom Pol1(mon1, size1);
     TPolinom Pol2(mon2, size2);
     TPolinom Pol3(mon3, size3);
@@ -517,10 +517,10 @@ TEST(TPolinom, can_subtract_simple_polinom) {
     int size1 = 3;
     int size2 = 1;
     int res_size = 3;
-    int mon1[][4] = { { 9, 0, 0, 4 } ,{ 8, 0, 0, 3 } ,
+    int mon1[][4] = { { 9, 0, 0, 4 } , { 8, 0, 0, 3 } ,
     { 5, 0, 0, 2 } };
     int mon2[][4] = { { 7, 0, 0, 4 } };
-    int resmon[][4] = { { 2, 0, 0, 4 } ,{ 8, 0, 0, 3 } ,
+    int resmon[][4] = { { 2, 0, 0, 4 } , { 8, 0, 0, 3 } ,
     { 5, 0, 0, 2 } };
     TPolinom first(mon1, size1);
     TPolinom second(mon2, size2);
@@ -536,11 +536,11 @@ TEST(TPolinom, can_subtract_simple_polinom) {
 TEST(TPolinom, can_subtract_polinom) {
     int size1 = 3;
     int res_size = 2;
-    int mon1[][4] = { { 9, 0, 0, 4 } ,{ 8, 0, 0, 3 } ,
+    int mon1[][4] = { { 9, 0, 0, 4 } , { 8, 0, 0, 3 } ,
     { 5, 0, 0, 2 } };
-    int mon2[][4] = { { 7, 0, 0, 4 } ,{ 8, 0, 0, 3 } ,
+    int mon2[][4] = { { 7, 0, 0, 4 } , { 8, 0, 0, 3 } ,
     { -2, 0, 0, 2 } };
-    int resmon[][4] = { { 2, 0, 0, 4 } ,{ 7, 0, 0, 2 } };
+    int resmon[][4] = { { 2, 0, 0, 4 } , { 7, 0, 0, 2 } };
     TPolinom first(mon1, size1);
     TPolinom second(mon2, size1);
     TPolinom res(resmon, res_size);
@@ -554,12 +554,12 @@ TEST(TPolinom, can_subtract_up_simple_polynoms_A_minus_B) {
     const int size1 = 3;
     const int size2 = 4;
     const int expected_size = 4;
-    int mon1[][4] = { { 9, 0, 0, 4 } ,{ 8, 0, 0, 3 } ,
+    int mon1[][4] = { { 9, 0, 0, 4 } , { 8, 0, 0, 3 } ,
     { 5, 0, 0, 2 } };
-    int mon2[][4] = { { 2, 0, 0, 5 } ,{ 7, 0, 0, 4 } ,
-    { 8, 0, 0, 3 } ,{ 1, 0, 0, 1 } };
-    int expected_mon[][4] = { { -2, 0, 0, 5 } ,{ 2, 0, 0, 4 } ,
-    { 5, 0, 0, 2 } ,{ -1, 0, 0, 1 } };
+    int mon2[][4] = { { 2, 0, 0, 5 } , { 7, 0, 0, 4 } ,
+    { 8, 0, 0, 3 } , { 1, 0, 0, 1 } };
+    int expected_mon[][4] = { { -2, 0, 0, 5 } , { 2, 0, 0, 4 } ,
+    { 5, 0, 0, 2 } , { -1, 0, 0, 1 } };
     TPolinom Pol1(mon1, size1);
     TPolinom Pol2(mon2, size2);
     TPolinom expected_Pol(expected_mon, expected_size);
@@ -573,12 +573,12 @@ TEST(TPolinom, can_subtract_up_simple_polynoms_B_minus_A) {
     const int size1 = 3;
     const int size2 = 4;
     const int expected_size = 4;
-    int mon1[][4] = { { 9, 0, 0, 4 } ,{ 8, 0, 0, 3 } ,
+    int mon1[][4] = { { 9, 0, 0, 4 } , { 8, 0, 0, 3 } ,
     { 5, 0, 0, 2 } };
-    int mon2[][4] = { { 2, 0, 0, 5 } ,{ 7, 0, 0, 4 } ,
-    { 8, 0, 0, 3 } ,{ 1, 0, 0, 1 } };
-    int expected_mon[][4] = { { 2, 0, 0, 5 } ,{ -2, 0, 0, 4 } ,
-    { -5, 0, 0, 2 } ,{ 1, 0, 0, 1 } };
+    int mon2[][4] = { { 2, 0, 0, 5 } , { 7, 0, 0, 4 } ,
+    { 8, 0, 0, 3 } , { 1, 0, 0, 1 } };
+    int expected_mon[][4] = { { 2, 0, 0, 5 } , { -2, 0, 0, 4 } ,
+    { -5, 0, 0, 2 } , { 1, 0, 0, 1 } };
     TPolinom Pol1(mon1, size1);
     TPolinom Pol2(mon2, size2);
     TPolinom expected_Pol(expected_mon, expected_size);
@@ -592,12 +592,12 @@ TEST(TPolinom, can_subtract_up_polynoms) {
     const int size1 = 5;
     const int size2 = 4;
     const int expected_size = 6;
-    int mon1[][4] = { { 10, 9, 9, 9 } ,{ -21, 5, 0, 0 } ,
-    { 10, 4, 3, 2 } ,{ 8, 3, 2, 1 } ,{ 5, 2, 1, 3 } };
-    int mon2[][4] = { { 20, 7, 0, 2 } ,{ 1, 5, 0, 0 } ,
-    { 8, 3, 2, 1 } ,{ 15, 0, 0, 0 } };
-    int expected_mon[][4] = { { 10, 9, 9, 9 } ,{ -20, 7, 0, 2 } ,
-    { -22, 5, 0, 0 } ,{ 10, 4, 3, 2 } ,{ 5, 2, 1, 3 } ,{ -15, 0, 0, 0 } };
+    int mon1[][4] = { { 10, 9, 9, 9 } , { -21, 5, 0, 0 } ,
+    { 10, 4, 3, 2 } , { 8, 3, 2, 1 } , { 5, 2, 1, 3 } };
+    int mon2[][4] = { { 20, 7, 0, 2 } , { 1, 5, 0, 0 } ,
+    { 8, 3, 2, 1 } , { 15, 0, 0, 0 } };
+    int expected_mon[][4] = { { 10, 9, 9, 9 } , { -20, 7, 0, 2 } ,
+    { -22, 5, 0, 0 } , { 10, 4, 3, 2 } , { 5, 2, 1, 3 } , { -15, 0, 0, 0 } };
     // 5x^2yz^3+8x^3y^2z+10x^4y^3z^2-21x^5+10x^9y^9z^9
     TPolinom Pol1(mon1, size1);
     // 15-8x^3y^2z+x^5+20x^7z^2
@@ -613,12 +613,12 @@ TEST(TPolinom, can_subtract_up_polynoms) {
 TEST(TPolinom, multiplication_with_simple_polinom) {
     int size1 = 4;
     int size2 = 1;
-    int mon1[][4] = { { 5, 3, 0, 6 } ,{ 4, 2, 4, 3 } ,
-    { 10, 2, 3, 5 } ,{ 10, 0, 0, 0 } };
+    int mon1[][4] = { { 5, 3, 0, 6 } , { 4, 2, 4, 3 } ,
+    { 10, 2, 3, 5 } , { 10, 0, 0, 0 } };
     int mon2[][4] = { { 2, 1, 1, 1 } };
     // 5*x^3*z^6 + 4*x^2*y^4*z^4 + 10*x^2*y^3*z^5 + 10
-    int resMon[][4] = { { 10, 4, 1, 7 } ,{ 8, 3, 5, 4 } ,
-    { 20, 3, 4, 6 } ,{ 20, 1, 1, 1 } };
+    int resMon[][4] = { { 10, 4, 1, 7 } , { 8, 3, 5, 4 } ,
+    { 20, 3, 4, 6 } , { 20, 1, 1, 1 } };
     TPolinom first(mon1, size1);
     // (2*x*y*z)
     TPolinom second(mon2, size2);
@@ -629,19 +629,19 @@ TEST(TPolinom, multiplication_with_simple_polinom) {
 TEST(TPolinom, multiplication_with_big_polinom_4x4) {
     int size = 4;
     int res_size = 16;
-    int mon1[][4] = { { 5, 3, 0, 6 } ,{ 5, 2, 4, 3 } ,
-    { 10, 2, 3, 5 } ,{ 10, 0, 0, 0 } };
-    int mon2[][4] = { { 2, 7, 5, 6 } ,{ 7, 5, 3, 8 } ,
-    { 12, 4, 6, 1 } ,{ 3, 4, 2, 0 } };
+    int mon1[][4] = { { 5, 3, 0, 6 } , { 5, 2, 4, 3 } ,
+    { 10, 2, 3, 5 } , { 10, 0, 0, 0 } };
+    int mon2[][4] = { { 2, 7, 5, 6 } , { 7, 5, 3, 8 } ,
+    { 12, 4, 6, 1 } , { 3, 4, 2, 0 } };
     int res_Mon[][4] = {
-        { 10, 10, 5, 12 } ,{ 10, 9, 9, 9 } ,
-        { 20, 9, 8, 11 } ,{ 35, 8, 3, 14 } ,
-        { 35, 7, 7, 11 } ,{ 70, 7, 6, 13 } ,
-        { 60, 7, 6, 7 } ,{ 20, 7, 5, 6 } ,
-        { 15, 7, 2, 6 } ,{ 60, 6, 10, 4 } ,
-        { 120, 6, 9, 6 } ,{ 15, 6, 6, 3 } ,
-        { 30, 6, 5, 5 } ,{ 70, 5, 3, 8 } ,
-        { 120, 4, 6, 1 } ,{ 30, 4, 2, 0 }
+        { 10, 10, 5, 12 } , { 10, 9, 9, 9 } ,
+        { 20, 9, 8, 11 } , { 35, 8, 3, 14 } ,
+        { 35, 7, 7, 11 } , { 70, 7, 6, 13 } ,
+        { 60, 7, 6, 7 } , { 20, 7, 5, 6 } ,
+        { 15, 7, 2, 6 } , { 60, 6, 10, 4 } ,
+        { 120, 6, 9, 6 } , { 15, 6, 6, 3 } ,
+        { 30, 6, 5, 5 } , { 70, 5, 3, 8 } ,
+        { 120, 4, 6, 1 } , { 30, 4, 2, 0 }
     };
     // 5*x^3*z^6 + 5*x^2*y^4*z^3 + 10*x^2*y^3*z^5 + 10
     TPolinom first(mon1, size);
