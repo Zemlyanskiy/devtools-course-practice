07bdedce2029dfd200c7d801120a2c9dff1cc53b
Umlore1998 <iv.kiselev1998@yandex.ru>
2017-05-17 10:36:20 +0300

    test change

--- modules/ivan-kiselev-polinom_calc/test/other_tests.cpp
+++ modules/ivan-kiselev-polinom_calc/test/other_tests.cpp
@@ -655,3 +655,132 @@ TEST(TPolinom, multiplication_with_big_polinom_4x4) {
 
     EXPECT_TRUE(result == &(first * &second));
 }
+
+//  Additional tests
+
+TEST(TDatList, DelCurrent) {
+    int mon[][4] = { { 1, 0, 1, 0 } ,{ 2, 0, 0, 2 } };
+    int resmon[][4] = { { 2, 0, 0, 2 } };
+    int size = 2;
+    int ressize = 1;
+    TPolinom pol(mon, size);
+    TPolinom res(resmon, ressize);
+
+    pol.Reset();
+    pol.DelCurrent();
+
+    EXPECT_TRUE(pol == &res);
+}
+
+TEST(TMonom, cout_test) {
+    TMonom* mon = new TMonom(2, 1, 1, 1);
+    GTEST_TEST_NO_FATAL_FAILURE_(std::cout << mon, GTEST_NONFATAL_FAILURE_);
+}
+
+TEST(TMonom, Equality1) {
+    TMonom* mon1 = new TMonom(2, 1, 1, 1);
+    TMonom* mon2 = new TMonom(3, 1, 1, 5);
+
+    EXPECT_FALSE(*mon2 < *mon1);
+}
+
+TEST(TMonom, Equality2) {
+    TMonom* mon1 = new TMonom(2, 1, 1, 1);
+    TMonom* mon2 = new TMonom(3, 1, 1, 5);
+
+    EXPECT_FALSE(*mon1 > *mon2);
+}
+
+TEST(TPolinom, Assignment_to_a_nonzero_polynom) {
+    int mon[][4] = { { 1, 0, 1, 0 } ,{ 2, 0, 0, 2 } };
+    int mon2[][4] = { { 1, 0, 1, 0 } ,{ 3, 0, 0, 2 } };
+    int size = 2;
+    TPolinom pol(mon, size);
+    TPolinom pol2(mon2, size);
+
+    pol2 = pol;
+
+    EXPECT_TRUE(pol == &pol2);
+}
+
+TEST(TPolinom, Not_Equal_polinoms1) {
+    int mon[][4] = { { 1, 0, 1, 0 } ,{ 2, 0, 0, 2 } };
+    int resmon[][4] = { { 2, 0, 0, 2 } };
+    int size = 2;
+    int ressize = 1;
+    TPolinom pol(mon, size);
+    TPolinom res(resmon, ressize);
+
+    EXPECT_FALSE(pol == &res);
+}
+
+TEST(TPolinom, Not_Equal_polinoms2) {
+    int mon[][4] = { { 1, 0, 1, 0 } ,{ 2, 0, 0, 2 } };
+    int resmon[][4] = { { 2, 0, 0, 2 } ,{ 10, 0, 0, 1 } };
+    int size = 2;
+    int ressize = 1;
+    TPolinom pol(mon, size);
+    TPolinom res(resmon, ressize);
+
+    EXPECT_FALSE(pol == &res);
+}
+
+TEST(TPolinom, cout_polinom) {
+    int mon[][4] = { { 1, 0, 1, 0 } ,{ 2, 0, 0, 2 } };
+    int size = 2;
+    TPolinom pol(mon, size);
+
+    GTEST_TEST_NO_FATAL_FAILURE_(std::cout << pol, GTEST_NONFATAL_FAILURE_);
+}
+
+TEST(THeadRing, can_create_THeadRing) {
+    GTEST_TEST_NO_FATAL_FAILURE_(THeadRing thead, GTEST_NONFATAL_FAILURE_);
+}
+
+TEST(TRootLink, can_create_TRootLink) {
+    TDatList list;
+
+    GTEST_TEST_NO_FATAL_FAILURE_(list.~TDatList(), GTEST_NONFATAL_FAILURE_);
+}
+
+TEST(TRootLink, can_set_next_link) {
+    TDatLink* link = new TDatLink();
+    TDatLink* TDL = new TDatLink();
+
+
+    TDL->SetNextLink(reinterpret_cast<TRootLink*> (link));
+    TRootLink* reslink = TDL->GetNextLink();
+
+    EXPECT_TRUE(reslink == link);
+}
+
+TEST(TDatList, list_ended) {
+    TDatList list;
+
+    int i = list.IsEmpty();
+
+    EXPECT_EQ(1, i);
+}
+
+TEST(TDatLink, SetDatValue_and_GetDatValue_and_GetNextDatLink) {
+    TDatLink MyLink;
+    TDatLink MyLink2(NULL, &MyLink);
+    TMonom mon(1, 0, 0, 0);
+
+    MyLink.SetDatValue(reinterpret_cast<TDatValue*> (&mon));
+
+    EXPECT_TRUE(mon == *(reinterpret_cast<TMonom*> (MyLink.GetDatValue())));
+    EXPECT_TRUE(&MyLink == MyLink2.GetNextDatLink());
+}
+
+TEST(TPolinom, mylt_on_int) {
+    int mon[][4] = { { 1, 0, 1, 0 } ,{ 2, 0, 0, 2 } };
+    int mon2[][4] = { { 2, 0, 1, 0 } ,{ 4, 0, 0, 2 } };
+    int size = 2;
+    TPolinom pol(mon, size);
+    TPolinom res(mon2, size);
+
+    pol = pol * size;
+
+    EXPECT_TRUE(pol == &res);
+}
