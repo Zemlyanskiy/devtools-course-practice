6256d650462251dba3491d7317bf37c1d1288d0f
Andrew Bakalin <bakalin.andrew@mail.ru>
2017-06-19 20:23:12 +0300

    Fixed application.h

--- modules/todo-list/include/application.h
+++ modules/todo-list/include/application.h
@@ -1,5 +1,10 @@
 // Copyright 2017 Bakalin Andrey
 
+#ifndef MODULES_TODO_LIST_INCLUDE_APPLICATION_H_
+#define MODULES_TODO_LIST_INCLUDE_APPLICATION_H_
+
 class Application {
-    
+
 };
+
+#endif  // MODULES_TODO_LIST_INCLUDE_APPLICATION_H_
