c24a7c9daab7a03d2f2d721f430219f24c0de11b
Nick Dmitrichev <dmitrichev.nick@gmail.com>
2017-06-19 12:46:23 +0300

    Style1.

--- modules/length_converter/include/LengthConverterApp.h
+++ modules/length_converter/include/LengthConverterApp.h
@@ -3,24 +3,24 @@
 #ifndef MODULES_LENGTH_CONVERTER_INCLUDE_LENGTHCONVERTERAPP_H_
 #define MODULES_LENGTH_CONVERTER_INCLUDE_LENGTHCONVERTERAPP_H_
 
-#include "include/length_converter.h"
 #include <string>
+#include "include/length_converter.h"
 
 class Application {
  public:
     Application();
     std::string operator()(int argc, const char** argv);
 
-private:
-	void help(const char* appname, const char* message = "");
-	bool validateNumberOfArguments(int argc, const char** argv);
-	std::string message_;
-	typedef struct {
-		double old_length_size;
-		LengthType old_length_name;
-		LengthType new_length_name;
-	} Arguments;
-	Length* length = new Length(CENTIMETRE, 1.0);
+ private:
+    void help(const char* appname, const char* message = "");
+    bool validateNumberOfArguments(int argc, const char** argv);
+    std::string message_;
+    typedef struct {
+        double old_length_size;
+        LengthType old_length_name;
+        LengthType new_length_name;
+    } Arguments;
+    Length* length = new Length(CENTIMETRE, 1.0);
 };
 
 #endif  // MODULES_LENGTH_CONVERTER_INCLUDE_LENGTHCONVERTERAPP_H_
