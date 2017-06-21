91100bbb8a003767c82d3eac49b1303fafde2b8f
Andrew Bakalin <bakalin.andrew@mail.ru>
2017-06-21 11:14:15 +0300

    Added test

--- modules/todo-list/test/test_application.cpp
+++ modules/todo-list/test/test_application.cpp
@@ -2,8 +2,47 @@
 
 #include <gtest/gtest.h>
 
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+
 #include "include/application.h"
 
-TEST(ApplicationTest, test123) {
-    EXPECT_EQ(1, 1);
+using ::testing::internal::RE;
+using std::vector;
+using std::string;
+
+class ApplicationTest : public ::testing::Test {
+ protected:
+    void Act(vector<string> args_) {
+        vector<const char*> options;
+
+        options.push_back("appname");
+        for (size_t i = 0; i < args_.size(); ++i) {
+            options.push_back(args_[i].c_str());
+        }
+
+        const char** argv = &options.front();
+        int argc = static_cast<int>(args_.size()) + 1;
+
+        output_ = app_(argc, argv);
+    }
+
+    void Assert(std::string expected) {
+        EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
+    }
+
+ private:
+    Application app_;
+    string output_;
+};
+
+TEST_F(ApplicationTest, test123) {
+    vector<string> args = {};
+
+    Act(args);
+
+    Assert("");
 }
