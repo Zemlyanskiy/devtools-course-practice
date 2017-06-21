5c65b9dfb22d3519c08198449cb33871c3408fdb
lemurtaison <zona131313@gmail.com>
2017-06-21 19:54:48 +0300

    fix style errors2

--- modules/deque/test/test_deque_text_editor_app.cpp
+++ modules/deque/test/test_deque_text_editor_app.cpp
@@ -13,28 +13,26 @@ using std::vector;
 using std::string;
 
 class  DequeTextEditorAppTest : public ::testing::Test {
-    protected:
+ protected:
         void Act(vector<string> args) {
             vector<const char*> options;
             
-                options.push_back("appname");
+            options.push_back("appname");
             for (size_t i = 0; i < args.size(); ++i) {
-                options.push_back(args[i].c_str()); 
+                options.push_back(args[i].c_str());
             }
             
-                const char** argv = &options.front();
+            const char** argv = &options.front();
             int argc = static_cast<int>(args.size()) + 1;
             
-                output_ = app_(argc, argv);
+            output_ = app_(argc, argv);
         }
-        
             void Assert(std::string expected) {
             EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
         }
-        
-            private:
-                DequeTextEditorApp app_;
-                string output_;
+ private:
+    DequeTextEditorApp app_;
+    string output_;
 };
 
 TEST_F(DequeTextEditorAppTest, can_print_help) {
@@ -98,7 +96,7 @@ TEST_F(DequeTextEditorAppTest,
     std::ofstream filestream(filename, std::ios_base::trunc);
     filestream << "teststring1" << std::endl;
     filestream.close();
-    vector<string> args = { "-lsfw", filename, "teststring2",};
+    vector<string> args = { "-lsfw", filename, "teststring2"};
 
     Act(args);
 
