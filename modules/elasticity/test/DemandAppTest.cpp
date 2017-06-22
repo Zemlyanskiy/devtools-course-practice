cf8408cca840993f18c570f824e7a4e53df89ed6
Bilkovsky <a.bilkov@yandex.ru>
2017-06-22 17:53:37 +0300

    fix 6

--- modules/elasticity/test/DemandAppTest.cpp
+++ modules/elasticity/test/DemandAppTest.cpp
@@ -14,49 +14,49 @@ using std::string;
 
 class DemandAppTest : public ::testing::Test {
 protected:
-	void Act(vector<string> args_) {
-		vector<const char *> options;
+    void Act(vector<string> args_) {
+        vector<const char *> options;
+        
+        options.push_back("appname");
+        for (size_t i = 0; i < args_.size(); ++i) {
+            options.push_back(args_[i].c_str());
+        }
+        
+        const char **argv = &options.front();
+        int argc = static_cast<int>(args_.size()) + 1;
+        
+        output_ = app_(argc, argv);
+    }
 
-		options.push_back("appname");
-		for (size_t i = 0; i < args_.size(); ++i) {
-			options.push_back(args_[i].c_str());
-		}
-
-		const char **argv = &options.front();
-		int argc = static_cast<int>(args_.size()) + 1;
-
-		output_ = app_(argc, argv);
-	}
-
-	void Assert(std::string expected) {
-		EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
-	}
-
+    void Assert(std::string expected) {
+        EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
+    }
+    
 private:
-	DemandApp app_;
-	string output_;
+    DemandApp app_;
+    string output_;
 };
 
 TEST_F(DemandAppTest, Do_Print_Help_Without_Arguments) {
-	std::vector<std::string> args = {};
-
-	Act(args);
-
-	Assert("This is an elasticity calculator*");
+    std::vector<std::string> args = {};
+    
+    Act(args);
+    
+    Assert("This is an elasticity calculator*");
 }
 
 TEST_F(DemandAppTest, Is_Checking_Number_Of_Arguments) {
-	vector<string> args = { "0.5", "3.0" };
+    vector<string> args = { "0.5", "3.0" };
+    
+    Act(args);
 
-	Act(args);
-
-	Assert("ERROR: Incorrect number of arguments");
+    Assert("ERROR: Incorrect number of arguments");
 }
 
 TEST_F(DemandAppTest, Is_Checking_Argument) {
-	vector<string> args = { "0.4", "0.9", "G", "7" };
+    vector<string> args = { "0.4", "0.9", "G", "7" };
 
-	Act(args);
-
-	Assert("Wrong number format");
+    Act(args);
+    
+    Assert("Wrong number format");
 }
