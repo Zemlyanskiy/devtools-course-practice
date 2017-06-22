5aa5a89bfa7d9ac6a1daded926457da3efcd9ff2
Bilkovsky <a.bilkov@yandex.ru>
2017-06-22 19:51:57 +0300

    fix 12

--- modules/elasticity/application/app.cpp
+++ modules/elasticity/application/app.cpp
@@ -1,6 +1,8 @@
 //  Copyright 2017 Bilkovsky ALexander
-#include "include/Demand_app.h"
+#include <stdio.h>
 #include <string>
+
+#include "include/Demand_app.h"
 
 int main(int argc, const char** argv) {
     DemandApp app;
