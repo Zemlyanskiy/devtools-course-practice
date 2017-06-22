803a2c605906baac1c6fcc520006565c28d58879
Bilkovsky <a.bilkov@yandex.ru>
2017-06-22 18:12:08 +0300

    fix 1224212

--- modules/elasticity/test/DemandAppTest.cpp
+++ modules/elasticity/test/DemandAppTest.cpp
@@ -13,41 +13,41 @@ using std::string;
 
 
 class DemandAppTest : public ::testing::Test {
-protected:
+ protected:
     void Act(vector<string> args_) {
         vector<const char *> options;
-        
+
         options.push_back("appname");
         for (size_t i = 0; i < args_.size(); ++i) {
             options.push_back(args_[i].c_str());
         }
-        
+
         const char **argv = &options.front();
         int argc = static_cast<int>(args_.size()) + 1;
-        
+
         output_ = app_(argc, argv);
     }
 
     void Assert(std::string expected) {
         EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
     }
-    
-private:
+
+ private:
     DemandApp app_;
     string output_;
 };
 
 TEST_F(DemandAppTest, Do_Print_Help_Without_Arguments) {
     std::vector<std::string> args = {};
-    
+
     Act(args);
-    
+
     Assert("This is an elasticity calculator*");
 }
 
 TEST_F(DemandAppTest, Is_Checking_Number_Of_Arguments) {
     vector<string> args = { "0.5", "3.0" };
-    
+
     Act(args);
 
     Assert("ERROR: Incorrect number of arguments");
@@ -57,6 +57,6 @@ TEST_F(DemandAppTest, Is_Checking_Argument) {
     vector<string> args = { "0.4", "0.9", "G", "7" };
 
     Act(args);
-    
+
     Assert("Wrong number format");
 }
