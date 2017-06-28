d3e53a5599c9615fd5a8e5743d3f2841f010ae75
Zemlyanskiy <nikita.nikita.000035@gmail.com>
2017-06-28 15:47:21 +0300

    Add tests for App

---
+++ modules/matrix-calculator/test/test_matrix_calculator_app.cpp
@@ -0,0 +1,52 @@
+// Copyright 2017 Grechukhin Nikita
+
+#include <gtest/gtest.h>
+
+#include <vector>
+#include <cmath>
+#include <string>
+#include <functional>
+
+#include "../include/matrix_calculator_app.h"
+
+using ::testing::internal::RE;
+
+class MatrixCalculatorAppTest : public ::testing::Test {
+protected:
+    void Act(const std::vector<std::string> &args_) {
+        std::vector<const char *> options;
+
+        options.push_back("appname");
+        for (size_t i = 0; i < args_.size(); ++i) {
+            options.push_back(args_[i].c_str());
+        }
+
+        const char **argv = &options.front();
+        int argc = static_cast<int>(args_.size()) + 1;
+
+        output_ = app_(argc, argv);
+    }
+
+    void Assert(const std::string &expected) {
+        EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
+    }
+private:
+    MatrixCalculatorApp app_;
+    std::string output_;
+};
+
+TEST_F(MatrixCalculatorAppTest, Do_Print_Help_Without_Arguments) {
+    std::vector<std::string> args = {};
+
+    Act(args);
+
+    Assert("This is a matrix calculator application\\..*");
+}
+
+TEST_F(MatrixCalculatorAppTest, Is_Checking_Number_Of_Arguments) {
+    std::vector<std::string> args = { "4", "4", "4", "4", "5.5", "7.4" };
+
+    Act(args);
+
+    Assert("This is a matrix calculator application\\..*");
+}
